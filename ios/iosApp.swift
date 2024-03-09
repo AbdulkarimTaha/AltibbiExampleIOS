4f939f26cb54a2d7dcc07b83fa19ad01fe322130
Abdulkarim Taha <kareem.taha@altibbi.com>
2024-03-09 16:43:37 +0300

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Abdulkarim Taha on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
